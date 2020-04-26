ce4a5f3248ef1a160daa50a0f815e4f9306c72c4
gen <gen1321@mail.ru>
2020-04-26 17:47:26 +0300

    add support for multiple hosts

--- lib/dockerex/client.ex
+++ lib/dockerex/client.ex
@@ -11,16 +11,17 @@ defmodule Dockerex.Client do
   end
 
   defp default_headers do
-    {:ok , hostname} = :inet.gethostname
+    {:ok, hostname} = :inet.gethostname()
     %{"Content-Type" => "application/json", "Host" => hostname}
   end
 
   @doc """
   Send a GET request to the Docker API at the speicifed resource.
   """
-  def get(resource, headers \\ default_headers, opt \\ []) do
-    Logger.debug "Sending GET request to the Docker HTTP API: #{resource}"
-    base_url <> resource
+  def get(host, resource, headers \\ default_headers, opt \\ []) do
+    Logger.debug("Sending GET request to the Docker HTTP API: #{resource}")
+
+    (host <> resource)
     |> HTTPoison.get!(headers, Keyword.merge(options, opt))
     |> decode_body
   end
@@ -28,30 +29,33 @@ defmodule Dockerex.Client do
   @doc """
   Send a POST request to the Docker API, JSONifying the passed in data.
   """
-  def post(resource, data \\ "", headers \\ default_headers, opt \\ []) do
-    Logger.debug "Sending POST request to the Docker HTTP API: #{resource}, #{inspect data}"
-    data = Poison.encode! data
-    base_url <> resource
-    |> HTTPoison.post!(data, headers, Keyword.merge(options, opt))
+  def post(host, resource, data \\ "", headers \\ default_headers, opt \\ []) do
+    Logger.debug("Sending POST request to the Docker HTTP API: #{resource}, #{inspect(data)}")
+    data = Poison.encode!(data)
+
+    (host <> resource)
+    |> HTTPoison.post!(data, headers, Keyword.merge(options(), opt))
     |> decode_body
   end
 
   @doc """
   Send a DELETE request to the Docker API.
   """
-  def delete(resource, headers \\ default_headers, opt \\ []) do
-    Logger.debug "Sending DELETE request to the Docker HTTP API: #{resource}"
-    base_url <> resource
+  def delete(host, resource, headers \\ default_headers, opt \\ []) do
+    Logger.debug("Sending DELETE request to the Docker HTTP API: #{resource}")
+
+    (host <> resource)
     |> HTTPoison.delete!(headers, Keyword.merge(options, opt))
   end
 
   defp decode_body(%HTTPoison.Response{body: ""}) do
-    Logger.debug "Empty response"
-    :nil
+    Logger.debug("Empty response")
+    nil
   end
 
   defp decode_body(%HTTPoison.Response{body: body}) do
-    Logger.debug "Decoding Docker API response: #{inspect body}"
+    Logger.debug("Decoding Docker API response: #{inspect(body)}")
+
     case Poison.decode(body) do
       {:ok, dict} -> dict
       {:error, _} -> body
