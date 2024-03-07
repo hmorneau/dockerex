eb24aa7544dfb68d5db4249a73b2a6a4ffc2e31b
hmorneau <me@hmorneau.com>
2024-03-07 10:57:53 -0500

    small fix fn and warning

--- lib/dockerex/client.ex
+++ lib/dockerex/client.ex
@@ -1,16 +1,16 @@
 defmodule Dockerex.Client do
   require Logger
 
-  defp base_url do
+  defp base_url() do
     host = Application.get_env(:dockerex, :host)
     "#{host}"
   end
 
-  defp options do
+  defp options() do
     Application.get_env(:dockerex, :options, [])
   end
 
-  defp default_headers do
+  defp default_headers() do
     {:ok, hostname} = :inet.gethostname()
     %{"Content-Type" => "application/json", "Host" => hostname}
   end
@@ -18,34 +18,34 @@ defmodule Dockerex.Client do
   @doc """
   Send a GET request to the Docker API at the speicifed resource.
   """
-  def get(host, resource, headers \\ default_headers, opt \\ []) do
+  def get(host, resource, headers \\ default_headers(), opt \\ []) do
     Logger.debug("Sending GET request to the Docker HTTP API: #{resource}")
 
     (host <> resource)
-    |> HTTPoison.get!(Map.merge(headers, default_headers), Keyword.merge(options, opt))
+    |> HTTPoison.get!(Map.merge(headers, default_headers()), Keyword.merge(options(), opt))
     |> decode_body
   end
 
   @doc """
   Send a POST request to the Docker API, JSONifying the passed in data.
   """
-  def post(host, resource, data \\ "", headers \\ default_headers, opt \\ []) do
+  def post(host, resource, data \\ "", headers \\ default_headers(), opt \\ []) do
     Logger.debug("Sending POST request to the Docker HTTP API: #{resource}, #{inspect(data)}")
     data = Poison.encode!(data)
 
     (host <> resource)
-    |> HTTPoison.post!(data, Map.merge(headers, default_headers), Keyword.merge(options(), opt))
+    |> HTTPoison.post!(data, Map.merge(headers, default_headers()), Keyword.merge(options(), opt))
     |> decode_body
   end
 
   @doc """
   Send a DELETE request to the Docker API.
   """
-  def delete(host, resource, headers \\ default_headers, opt \\ []) do
+  def delete(host, resource, headers \\ default_headers(), opt \\ []) do
     Logger.debug("Sending DELETE request to the Docker HTTP API: #{resource}")
 
     (host <> resource)
-    |> HTTPoison.delete!(Map.merge(headers, default_headers), Keyword.merge(options, opt))
+    |> HTTPoison.delete!(Map.merge(headers, default_headers()), Keyword.merge(options(), opt))
   end
 
   defp decode_body(%HTTPoison.Response{body: ""}) do
